470f5996b530677eb6b66bf12fe9cd14c3927838
shenylin <chlin7@illinois.edu>
2023-04-21 09:45:12 -0500

    Add files via upload

---
+++ ANOVA_20180321.R
@@ -0,0 +1,109 @@
+#高統ANOVA上機
+#2018.03.21
+
+##1.讀取檔案 
+INDAOV.raw <- read.csv(file.choose(), header=TRUE)
+
+##2.檢查資料
+head(INDAOV.raw) #view(Indaov.raw)
+
+##3.選取要分析的變項
+INDAOV<-INDAOV.raw[c(2,3,12)]
+INDAOV
+
+##4.基本統計資訊
+summary(INDAOV)
+
+##5.資料型態
+str(INDAOV) #class(INDAOV$ID)
+
+##6.轉換資料型態
+INDAOV$ID <-as.factor(INDAOV$ID)
+class(INDAOV$ID)
+
+##7.1描述性統計
+library(psych)
+describe(INDAOV[2:3])
+
+##7.2變異數同質性檢定 (依~自, data=資料)
+bartlett.test( reading_3 ~ ethnic, data=INDAOV)
+
+#levene test 
+library(car)
+leveneTest(reading_3 ~ ethnic, data=INDAOV)
+
+##8.ANOVA分析
+INDAOV.op  <- aov(reading_3 ~ ethnic, data=INDAOV)
+summary(INDAOV.op)
+
+##9.繪製圖形
+library(car)
+library(sandwich)
+library(RcmdrMisc)
+with(INDAOV, plotMeans(reading_3, ethnic, error.bars="se"))
+with(INDAOV, boxplot  (reading_3 ~ethnic, error.bars="se"))
+title(xlab="ethnic", ylab="score")
+
+
+##10.進行事後比較
+TukeyHSD(INDAOV.op)
+
+#Scheffe.test
+library(agricolae)
+sch<-scheffe.test(INDAOV.op,"ethnic",group=F)
+sch$comparison
+
+library(userfriendlyscience)
+oneway(INDAOV$ethnic, y = INDAOV$reading_3, posthoc = 'games-howell')
+oneway(INDAOV$ethnic, y = INDAOV$reading_3, posthoc = 'bonferroni')
+
+#--------------------------------------------------------------
+#RM-ANOVA
+##1.
+RMaov.raw<- read.csv(file.choose())
+RMaov<-RMaov.raw[c(12,17,22)]
+head(RMaov)
+#2.資料轉換 多變量->單變量
+RMaov.U<-stack(RMaov)
+head(RMaov.U)
+#3.
+RMaov.U$RM_id = rep(RMaov.raw$ID, 3)
+#4.
+colnames(RMaov.U) = c("RM_read_score", "RM_time", "RM_id")
+head(RMaov.U)
+
+#5.同質性檢定
+bartlett.test(RM_read_score ~ RM_time, data=RMaov.U)
+
+library(car)
+leveneTest(RM_read_score ~ RM_time, data=RMaov.U)
+
+#6.繪製平均數圖
+library(car)
+library(sandwich)
+library(RcmdrMisc)
+with(RMaov.U,plotMeans(RM_read_score,RM_time,error.bars="se"))
+
+#7.1設定因子 & 排序
+time <- ordered(  c("reading_1", "reading_2", "reading_3"),
+                  levels = c("reading_1", "reading_2", "reading_3"))
+time<-data.frame(time)
+time
+
+#7.2將RMaov轉matrix格式，並建立模型
+RMaov <- as.matrix(RMaov)
+RMaov.model <- lm(RMaov ~ 1)
+
+#7.3進行分析
+RM<-Anova(RMaov.model, idata = time,idesign = ~time)
+summary(RM)
+
+
+#8.事後比較
+library(MASS)
+library(nlme)
+library(multcomp)
+
+Lme.mod<-lme(RM_read_score ~ RM_time,random = ~1|RM_id/RM_time,data = RMaov.U)
+summary(glht(Lme.mod, linfct=mcp(RM_time="Tukey")))
+
---
+++ Path analysis R_20180530.R
@@ -0,0 +1,60 @@
+#中介、調節 Path Analysis R
+#2018.05.30
+
+## 中介
+# 讀檔
+PA.RAW <- read.csv(file.choose())
+
+#多元共線性檢定
+library(car)
+MED.test<-lm(reading_3 ~ eng_1 + WA_S_2, data=PA.RAW)
+vif(MED.test)
+
+# 建立中介路徑模型
+library(lavaan)
+MED.model <- ' WA_S_2 ~ a*eng_1
+              reading_3 ~ c*eng_1 + b*WA_S_2
+              indirect := a*b
+              total    := c+(a*b) '
+
+# 利用sem進行模型設定
+fit.3 <- sem(MED.model, sample.nobs = 193, data = PA.RAW)
+
+# 利用summary進行中介模型分析
+MED.OP <- summary(fit.3, fit.measures=T, rsquare=T, standardized=T)
+
+# 利用semPaths繪製模型圖
+library(semPlot)
+semPaths(fit.3, whatLabels = "std", layout = "spring",
+         sizeMan = 10,sizeLat=10,nCharNodes=10,edge.label.cex = 1)
+----------------------------------------------------
+## 調節
+PA.mod <- PA.RAW[1:129, c(3,13,25)]
+str(PA.mod)
+
+# 轉換
+library(dplyr)
+PA.mod<-mutate(PA.mod,ethnic_h = ifelse(ethnic == "Human", 1, 0))
+str(PA.mod)
+
+# 調節
+MOD.OP<-lm( math_3 ~ WA_S_1*ethnic_h, data = PA.mod)
+summary(MOD.OP)
+
+# 交互作用顯著，針對ethnic_h下水準進行迴歸分析
+
+## @ human
+MOD.OP.h <- lm( math_3 ~ WA_S_1, data = PA.mod[1:87,])
+summary(MOD.OP.h)
+
+## @ orc
+MOD.OP.o <- lm( math_3 ~ WA_S_1, data = PA.mod[88:129,])
+summary(MOD.OP.o)
+  
+# 利用ggplot繪製模型圖
+library(ggplot2)
+ggplot(PA.mod , aes(x = WA_S_1, y =math_3, group=ethnic)) +
+              geom_point(size=3) +
+              stat_smooth(method="lm",aes(color = ethnic))
+
+
